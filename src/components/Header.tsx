3d223909301cfcdfafaf0e8302407f235908879b
GUILLAUME ZIMBERLIN <zimberlinguillaume@gmail.com>
2025-05-21 17:56:11 +0200

    Header + css

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+function Header() {
+	return (
+		<section className="flex">
+			<img
+				src="\src\assets\images\Avatar-header.webp"
+				alt="Une mascotte d'ordinateur vert tout mimi"
+				className="w-[100px] h-auto"
+			/>
+			<h1 className="text-6xl ">AlgoLinguo</h1>
+		</section>
+	);
+}
+
+export default Header;
