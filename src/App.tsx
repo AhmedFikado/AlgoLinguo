2a155e205e7e0c3f3fa327db4c4e6fae19bc8d1a
GUILLAUME ZIMBERLIN <zimberlinguillaume@gmail.com>
2025-05-21 19:44:12 +0200

    Added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { Outlet } from "react-router";
 import "./App.css";
 import Header from "./components/Header";
 
 function App() {
 	return (
-		<>
+		<section>
 			<Header />
-		</>
+			<main>
+				<Outlet />
+			</main>
+		</section>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router";
+
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// import About from "./pages/About";
+// import Home from "./pages/Home";
+
+
+/* ************************************************************************* */
+
+// const router = createBrowserRouter([
+//   {
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/About",
+//         element: <About />,
+//       },
+//     ],
+//   }, 
+// ]);
+
+/* ************************************************************************* */
+
+// Find the root element in the HTML document
+const rootElement = document.getElementById("root");
+if (rootElement == null) {
+  throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
+}
+
+// Render the app inside the root element
+// if (rootElement != null) {
+//   ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+// }
