81b61dcf579f9dd73430bdd49bce3182e7a1a6a0
ahmed firoum <ahmedfiroumk@gmail.com>
2025-05-22 14:22:10 +0200

    fixbug image done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,17 @@ import { Link } from "react-router";
 
 function Header() {
 	return (
-			<Link to="/" className="flex items-center justify-center bg-secondary h-[100px] cursor-pointer"><img
-				src="\src\assets\images\Avatar-header.webp"
+		<Link
+			to="/"
+			className="flex items-center justify-center bg-secondary h-[100px] cursor-pointer"
+		>
+			<img
+				src="\images\Avatar-header.webp"
 				alt="Une mascotte d'ordinateur vert tout mimi"
 				className="w-[100px] h-auto"
 			/>
-			<h1 className="text-6xl text-primary">AlgoLinguo</h1> </Link>
+			<h1 className="text-6xl text-primary">AlgoLinguo</h1>{" "}
+		</Link>
 	);
 }
 
